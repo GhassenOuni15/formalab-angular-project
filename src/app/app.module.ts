6c2d2ad8ab43452f5763d0cfb8e7ad0047cd4e9f
Ouni Ghassen <ouni0ghassen@gmail.com>
2022-08-08 08:56:35 +0100

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { PeopleListComponent } from './people-list/people-list.component';
+import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { Page404Component } from './page404/page404.component';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    PeopleListComponent,
+    RegisterComponent,
+    NavbarComponent,
+    FooterComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule,
+     HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
